ec2b39580b09053aa4584e43d0bbdf19839da6e6
Vincent Wang <vinowang921@gmail.com>
2023-05-31 11:49:22 -0400

    fdsafdsa

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-#  Web VPython 3.2
-from vpython import *
+  Web VPython 3.2
+#from vpython import *
 #Constants
 runRate = 100 #50 times a second
 elasticity = 1
@@ -75,11 +75,10 @@ class ForceCreator:
         self.shape.pos = self.position
 
 class Charges:
-    def __init__(self, position, charge, chargeColor, showField):
+    def __init__(self, position, charge, chargeColor):
         self.position = position
         self.charge = charge
         self.shape = cylinder(pos=self.position, axis=vector(0,0,1), radius=5, color=chargeColor)
-        self.showField = showField
     def calculateElectricField(self, point):
         distanceVector = self.position - point
         forceMag = (1 / (4 * pi* 8.85 * pow(10, -12))) * (self.charge / pow(mag(distanceVector), 2))
@@ -159,13 +158,12 @@ def mouseDownEventHandler():
         mouse.currCharge = "Negative"
 def mouseUpEventHandler():
     if (mouse.picked and mouse.currCharge == "Positive"):
-        forceCreatorsList.append(Charges(vector(scene.mouse.pos.x, scene.mouse.pos.y, 0), 1, color.red, True))
+        forceCreatorsList.append(Charges(vector(scene.mouse.pos.x, scene.mouse.pos.y, 0), 1, color.red))
         mouse.picked = False
     elif (mouse.picked and mouse.currCharge == "Negative"):
-        forceCreatorsList.append(Charges(vector(scene.mouse.pos.x, scene.mouse.pos.y, 0), -1, color.blue, True))
+        forceCreatorsList.append(Charges(vector(scene.mouse.pos.x, scene.mouse.pos.y, 0), -1, color.blue))
         mouse.picked = False
     mouse.currCharge = "None"
-    
 
 
 #Declarations
@@ -177,7 +175,7 @@ negativeChargeHolder = ChargeHolder(vector(120, 130, 0), -1)
 mouse = StupidMouse()
 
 forceCreatorsList = []
-arrowList = []
+showField = True
 
 scene.bind("mousedown", mouseDownEventHandler)
 scene.bind("mouseup", mouseUpEventHandler)
@@ -187,15 +185,14 @@ while(True):
         rate(runRate)
         for object in forceCreatorsList:
             puck.update(object)
-        if (self.showField):
+        arrowList = []
+        if (showField and len(forceCreatorsList) > 0):
             for x in range(-200, 200, 20):
                 for y in range(-200, 200, 20):
                     forceVector = vector(0,0,0)
                     for object in forceCreatorsList:
                         forceVector = forceVector + object.calculateElectricField(vector(x, y, 0))
                     arrowList.append(arrow(pos=vector(x, y, 0), axis=forceVector, color=color.green, length = 15))
-        else:
-            arrowList = []
         forcer.update()
         goal.inGoal(puck)
     #when click and drag add a new point charge to a list, then run the loop through puck and update everyone
