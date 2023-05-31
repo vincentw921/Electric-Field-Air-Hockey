db1df9b3b8d28a4627e7870526c828ea03beb4bf
Vincent Wang <vinowang921@gmail.com>
2023-05-31 08:45:05 -0400

    BOOOM

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Web VPython 3.2
 from vpython import *
 #Constants
-runRate = 50 #50 times a second
+runRate = 100 #50 times a second
 elasticity = 1
 gameMode = "Simulation"
 #Background
@@ -32,6 +32,7 @@ class Puck:
            
         distanceVector = self.position - forceCreators.position
         forceMag = (1 / (4 * pi* 8.85 * pow(10, -12))) * ((self.charge * forceCreators.charge) / pow(mag(distanceVector), 2))
+        
         forceVector = vector((forceMag * distanceVector.x) / mag(distanceVector), (forceMag * distanceVector.y) / mag(distanceVector), 0) 
         self.netForce = self.netForce + forceVector
             
@@ -75,12 +76,22 @@ class ForceCreator:
         self.shape.pos = self.position
 
 class Charges:
-    def __init__(self, position, charge):
+    def __init__(self, position, charge, chargeColor, showField):
         self.position = position
         self.charge = charge
-        self.shape = cylinder(pos=self.position, axis=vector(0,0,1), radius=5, color=color.red)
+        self.shape = cylinder(pos=self.position, axis=vector(0,0,1), radius=5, color=chargeColor)
+        self.showField = showField
+    def calculateElectricField(self, point):
+        distanceVector = self.position - point
+        forceMag = (1 / (4 * pi* 8.85 * pow(10, -12))) * (self.charge / pow(mag(distanceVector), 2))
+        forceVector = vector((forceMag * distanceVector.x) / mag(distanceVector), (forceMag * distanceVector.y) / mag(distanceVector), 0) 
+        return forceVector
     def createElectricField(self):
-        return 0  
+        if (self.showField):
+            for x in range(-200, 200, 10):
+                for y in range(-200, 200, 10):
+                    forceVector = self.calculateElectricField(self, vector(x, y, 0))
+                    arrow(pos=vector(x, y, 0), axis=forceVector, color=color.green)
 
 class Obstacles:
     def __init__(self, position, charge):
@@ -108,16 +119,16 @@ class ChargeHolder():
         self.position = position
         self.charge = charge
         self.shape = box(pos=self.position, length = 20, width = 1, height = 20, color = color.cyan)
-        # self.color
+        self.color = color.blue
         # if (charge > 0):
         #     self.color = color.red
         # else:
         #     self.color = color.blue
-        # self.circles = [cylinder(pos=self.position - vector(5, 5, 0), axis=vector(0,0,1), radius=2, color=self.color), cylinder(pos=self.position+vector(5, 5, 0), axis=vector(0,0,1), radius=2, color=self.color)]
+        self.circles = [cylinder(pos=self.position - vector(5, 5, 0), axis=vector(0,0,1), radius=2, color=self.color), cylinder(pos=self.position+vector(5, 5, 0), axis=vector(0,0,1), radius=2, color=self.color)]
 class StupidMouse():
     def __init__(self):
         self.picked = False
-        
+        self.currCharge = None
 
 def ray_tracing_method(x,y,poly):
     print(poly)
@@ -149,20 +160,29 @@ def mouseDownEventHandler():
     #if mouse is in the charge box and mousedown is pressed
     if (scene.mouse.pos.x < positiveChargeHolder.position.x + 10 and scene.mouse.pos.x > positiveChargeHolder.position.x - 10 and scene.mouse.pos.y < positiveChargeHolder.position.y + 10 and scene.mouse.pos.y > positiveChargeHolder.position.y - 10):
         mouse.picked = True
-        print("dong")
+        mouse.currCharge = "Positive"
+    if (scene.mouse.pos.x < negativeChargeHolder.position.x + 10 and scene.mouse.pos.x > negativeChargeHolder.position.x - 10 and scene.mouse.pos.y < negativeChargeHolder.position.y + 10 and scene.mouse.pos.y > negativeChargeHolder.position.y - 10):
+        mouse.picked = True
+        mouse.currCharge = "Negative"
 def mouseUpEventHandler():
-    if (mouse.picked):
-        forceCreatorsList.append(Charges(vector(scene.mouse.pos.x, scene.mouse.pos.y, 0), 1))
+    if (mouse.picked and mouse.currCharge == "Positive"):
+        forceCreatorsList.append(Charges(vector(scene.mouse.pos.x, scene.mouse.pos.y, 0), 1, color.red, True))
         mouse.picked = False
+    elif (mouse.picked and mouse.currCharge == "Negative"):
+        forceCreatorsList.append(Charges(vector(scene.mouse.pos.x, scene.mouse.pos.y, 0), -1, color.blue, True))
+        mouse.picked = False
+    mouse.currCharge = "None"
+    
+
 
 #Declarations
-puck = Puck(1, vector(-10, 0, 0), vector(20, 20, 0), pow(10, -4), 5)
-forcer = ForceCreator(vector(0, 20, 0), pow(10, -4))
+puck = Puck(1, vector(-10, 0, 0), vector(20, 20, 0), pow(10, -5), 5)
+forcer = ForceCreator(vector(0, 20, 0), pow(10, -5))
 goal = Goal(vector(150, 0, 0))
 positiveChargeHolder = ChargeHolder(vector(100, 130, 0), 1)
 negativeChargeHolder = ChargeHolder(vector(120, 130, 0), -1)
 mouse = StupidMouse()
-
+charge = Charges(vector(10, 0, 0), 1, color.red, False)
 forceCreatorsList = []
 
 scene.bind("mousedown", mouseDownEventHandler)
@@ -173,9 +193,11 @@ while(True):
         rate(runRate)
         for object in forceCreatorsList:
             puck.update(object)
-        
+        puck.update(charge)
         forcer.update()
-        goal.inGoal(forcer)
+        goal.inGoal(puck)
+        break
+    break
     #when click and drag add a new point charge to a list, then run the loop through puck and update everyone
    
     
